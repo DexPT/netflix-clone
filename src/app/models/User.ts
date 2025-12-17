9c6e1946d9471d20e9feedc4ea7935b37ec5cbfa
Roberto Ruivo <roberto.ruivo@lynxmind.com>
2025-12-17 16:03:50 +0000

    feat: add user model with password hashing

---
+++ src/app/models/User.ts
@@ -0,0 +1,38 @@
+import bcrypt from "bcryptjs";
+import { model, models, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    favorites: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Movie",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.password && this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
+
+const User = models?.User || model("User", userSchema);
+
+export default User;

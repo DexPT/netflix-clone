5163f17a24ee9020eeb35aba3c59f5225d95a2a8
Roberto Ruivo <roberto.ruivo@lynxmind.com>
2025-12-17 15:15:20 +0000

    chore: add db connection

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,17 +1,47 @@
+"use client";
+
 import { Input } from "@/shared/ui/Input";
 import Link from "next/link";
+import { useState } from "react";
 import { BsGithub } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = () => {};
+
   return (
     <div className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center bg-black loginContainer">
       <div className="max-w-[480px] w-full bg-[#000000b3] rounded-sm py-12 px-16 font-bold text-[2rem] text-white flex flex-col gap-5 z-50">
         <h1>Sign Up</h1>
-        <Input type="name" placeholder="Username" className="py-6 px-2" />
-        <Input type="email" placeholder="Email" className="py-6 px-2" />
-        <Input type="password" placeholder="Password" className="py-6 px-2" />
-        <button className="cursor-pointer w-full bg-[#e50914] text-base font-medium rounded-lg py-2.5">
+        <Input
+          type="name"
+          placeholder="Username"
+          className="py-6 px-2"
+          name={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          type="email"
+          placeholder="Email"
+          className="py-6 px-2"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          className="py-6 px-2"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="cursor-pointer w-full bg-[#e50914] text-base font-medium rounded-lg py-2.5"
+          onClick={handleSignUp}
+        >
           Sign Up
         </button>
         <p className="text-base text-[#ffffffb3] text-center ">OR</p>

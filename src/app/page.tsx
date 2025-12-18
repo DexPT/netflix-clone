9ec7b45cb60008b128335a911974d8832051d36c
Roberto Ruivo <roberto.ruivo@lynxmind.com>
2025-12-18 19:31:23 +0000

    feat: Add Navbar and DropdownMenu components and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Navbar from "@/shared/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }

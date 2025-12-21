ff4cbe946a04eb55e94b1293ca8e889077db7420
Roberto Ruivo <roberto.ruivo@lynxmind.com>
2025-12-21 12:14:06 +0000

    feat: implement user favorites and profile api endpoints

---
+++ src/app/api/me/route.ts
@@ -0,0 +1,15 @@
+import serverAuth from "@/lib/serverAuth";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const { currentUser } = await serverAuth();
+
+    return NextResponse.json({ currentUser }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
